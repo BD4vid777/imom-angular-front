6da774ccd63d73bfdd211a6178f3ef5c069ae49c
Dawid <64140934+BD4vid777@users.noreply.github.com>
2021-05-09 00:24:58 +0200

    Food Added

--- src/app/home/food-dash/food-dash.component.ts
+++ src/app/home/food-dash/food-dash.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoodDashComponent implements OnInit {
 
+  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  meals = ['Breakfast', '2nd Breakfast', 'Lunch', 'Dinner', 'Supper'];
+
+  daySelected = '';
+  mealSelected = '';
+
+  foodList = [
+    {name: "Scrumbled Eggs", description: "Scrumble and pan", meal: "Breakfast", day: ['Monday', "Friday", 'Saturday']},
+    {name: "Oat Meal", description: "Mix with milk", meal: "Breakfast", day: ['Monday', "Tuesday', 'Wednesday", 'Sunday']},
+    {name: "Scrumbled Eggs", description: "Scrumble and pan", meal: "2nd Breakfast", day: ['Monday', "Friday", 'Saturday']},
+    {name: "Scrumbled Eggs", description: "Scrumble and pan", meal: "2nd Breakfast", day: ['Monday', "Thursday", 'Saturday']},
+    {name: "Scrumbled Eggs", description: "Scrumble and pan", meal: "Lunch", day: ['Monday', "Friday", 'Saturday']},
+    {name: "Scrumbled Eggs", description: "Scrumble and pan", meal: "Lunch", day: ['Monday', "Friday", 'Saturday']},
+    {name: "Scrumbled Eggs", description: "Scrumble and pan", meal: "Dinner", day: ['Monday', "Friday", 'Saturday']},
+    {name: "Scrumbled Eggs", description: "Scrumble and pan", meal: "Dinner", day: ['Monday', "Friday", 'Saturday']},
+    {name: "Scrumbled Eggs", description: "Scrumble and pan", meal: "Dinner", day: ['Monday', "Friday", 'Saturday']},
+    {name: "Scrumbled Eggs", description: "Scrumble and pan", meal: "Supper", day: ['Monday', "Friday", 'Saturday']},
+    {name: "Scrumbled Eggs", description: "Scrumble and pan", meal: "Supper", day: ['Monday', "Friday", 'Saturday']},
+    {name: "Scrumbled Eggs", description: "Scrumble and pan", meal: "Supper", day: ['Monday', "Friday", 'Saturday']},
+    ]
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  selectDay(day: string) {
+    this.daySelected = day;
+    console.log(this.daySelected);
+  }
+
+  selectMeal(meal: string) {
+    this.mealSelected = meal;
+    console.log(this.mealSelected);
+  }
 }

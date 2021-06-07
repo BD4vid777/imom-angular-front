4809620695f1f86bec90da21884eba19e01c7a24
valerilysytsya <lashark11@gmail.com>
2021-06-07 15:59:23 +0300

    components update: home and blog

--- src/app/nav/calendar/calendar.component.ts
+++ src/app/nav/calendar/calendar.component.ts
@@ -1,21 +1,10 @@
-import {Component, Inject, Input, NgZone, OnInit} from '@angular/core';
-import {sampleOne} from './sample-data';
-import {Component, Inject, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CalendarService} from './service/calendar-task.service';
 import {EventCalendar} from './model/eventCalendar';
 import {Task} from "./model/task";
-import {EditTaskComponent} from "../../home/calendar-dash/edit-task/edit-task.component";
-import {HomeService} from '../../home/service/home.service';
 import {MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
-import {NewTaskComponent} from '../../home/calendar-dash/new-task/new-task.component';
 import {NewEventComponent} from './new-event/new-event.component';
-
-import {Task} from './model/task';
 import {EditTaskComponent} from '../../home/calendar-dash/edit-task/edit-task.component';
-import {HomeService} from '../../home/service/home.service';
-import {MAT_DIALOG_DATA, MatDialog} from '@angular/material/dialog';
-import {NewTaskComponent} from '../../home/calendar-dash/new-task/new-task.component';
-import {NewEventComponent} from './new-event/new-event.component';
 
 
 @Component({
@@ -57,42 +46,42 @@ export class CalendarComponent implements OnInit {
     this.calendarService.getTasks().subscribe(task => this.tasks = task);
   }
 
-  deleteTask(task: Task) {
-    const index: number = this.tasks.indexOf(task);
-    if (index !== -1) {
-      this.tasks.splice(index, 1);
-    }
-    this.calendarService.deleteTask(task.id).subscribe();
-
-  }
-
-  editingTask(task: Task) {
-    const dialogRef = this.dialog.open(EditTaskComponent, {
-      width: '400px',   data: {editingTask: task}}
-    );
-  }
-
-  toggleStatus(task: Task) {
-    task = {
-      id: task.id,
-      taskName: task.taskName,
-      taskText: task.taskText,
-      taskStatus: !task.taskStatus,
-    };
-    this.calendarService.editTask(task).subscribe();
-
-  }
-
-  addEvent() {const dialogRef = this.dialog.open(NewEventComponent, {
-    width: '400px'});
-              dialogRef.afterClosed().subscribe(result => {
-      window.location.reload();
-    });
-  }
-
-  onChange(){
-    this.ngOnInit();
-  }
+  // deleteTask(task: Task) {
+  //   const index: number = this.tasks.indexOf(task);
+  //   if (index !== -1) {
+  //     this.tasks.splice(index, 1);
+  //   }
+  //   this.calendarService.deleteTask(task.id).subscribe();
+  //
+  // }
+  //
+  // editingTask(task: Task) {
+  //   const dialogRef = this.dialog.open(EditTaskComponent, {
+  //     width: '400px',   data: {editingTask: task}}
+  //   );
+  // }
+  //
+  // toggleStatus(task: Task) {
+  //   task = {
+  //     id: task.id,
+  //     taskName: task.taskName,
+  //     taskText: task.taskText,
+  //     taskStatus: !task.taskStatus,
+  //   };
+  //   this.calendarService.editTask(task).subscribe();
+  //
+  // }
+  //
+  // addEvent() {const dialogRef = this.dialog.open(NewEventComponent, {
+  //   width: '400px'});
+  //             dialogRef.afterClosed().subscribe(result => {
+  //     window.location.reload();
+  //   });
+  // }
+  //
+  // onChange(){
+  //   this.ngOnInit();
+  // }
 }
 
 
